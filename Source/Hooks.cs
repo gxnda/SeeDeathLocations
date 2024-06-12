14f847979a14ec6c64e5d5188dd53038ae7dd266
Gabriel Lancaster-West <gabriellancasterwest@gmail.com>
2024-06-12 21:46:40 +0100

    Created ability to display death position in logs (untested), still have not made ability to save death position.
    Signed-off-by: Gabriel Lancaster-West <gabriellancasterwest@gmail.com>

--- Source/Hooks.cs
+++ Source/Hooks.cs
@@ -1,20 +1,30 @@
-﻿using System;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 
 namespace Celeste.Mod.SeeDeathLocations {
     public class Hooks {
         internal static void Load() {
-            On.Celeste.Player.Die += ModSaveAndDisplayDeathCoordinates;
+            On.Celeste.Player.Die += ModSaveAndDisplayDeathPosition;
         }
 
-        private static PlayerDeadBody ModSaveAndDisplayDeathCoordinates(Celeste.Player.Die orig,
-            Vector2 direction, bool evenIfInvincible = false, bool registerDeathInStats = true) {
+        private static PlayerDeadBody ModSaveAndDisplayDeathPosition(
+            On.Celeste.Player.orig_Die orig,
+            Player self,
+            Vector2 direction,
+            bool evenIfInvincible = false,
+            bool registerDeathInStats = true
+        ) {
+            DisplayDeathPosition(self);
+            SaveDeathPosition(self);
             // Do original behaviour afterwards
-            float x = self.X;
-            float y = self.Y;
-            Tuple<float, float> position = new Tuple<float, float>(x, y);
-            Logger.Log(position);
-            return orig(direction, evenIfInvincible, registerDeathInStats);
+            return orig(self, direction, evenIfInvincible, registerDeathInStats);
+        }
+
+        private static void SaveDeathPosition(Player self) {
+            Logger.Log(nameof(SeeDeathLocations), "Has not been created yet!");
+        }
+
+        private static void DisplayDeathPosition(Player self) {
+            Logger.Log(nameof(SeeDeathLocations), $"Death at: {self.Position}");
         }
     }
 }
